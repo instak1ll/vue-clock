86d499d93a28e6d4fb6217194e046486e76cf1d3
Artem Gladkov <artemgladkovvvv@gmail.com>
2023-03-16 04:13:09 +0100

    Un reloj digital que muestra datos #1
    Añanado 3 variables, despues función con methods 'updateclock'
    creo dentro otra variable con new Data para asignarle esos datos a las 3 variables del principio.
    Despues hago un mounted con setInterval para que actualice cada segundo el new Data.

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      data: new Date(),
+      horas: 0,
+      minutos: 0,
+      segundos: 0,
+    };
+  },
+  methods: {
+    updateClock() {
+      const date = new Date();
+      this.horas = String(date.getHours()).padStart(2, '0');
+      this.minutos = String(date.getMinutes()).padStart(2, '0');
+      this.segundos = String(date.getSeconds()).padStart(2, '0');
+    }
+  },
+  mounted() {
+    this.updateClock();
+    setInterval(this.updateClock, 1000)
+  }
+}
+</script>
+
+<template>
+  <body>
+    <span>{{ this.horas }} : {{ this.minutos }} : {{ this.segundos }}</span>
+  </body>
+</template>
+
+<style scoped>
+span {
+  font-size: 8vw;
+  color: lightcoral;
+  background-color: rgba(0, 0, 0, 0.8);
+  border-radius: 10px;
+  padding: 0 30px;
+}
+
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
